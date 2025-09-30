4d577582062881d4060de8be92b269997259fd06
Vishal Nagar <vishal19.nagar@tataaig.com>
2025-09-30 15:22:15 +0530

    first commit

---
+++ src/main/java/app/entity/employee/User.java
@@ -0,0 +1,8 @@
+package app.entity.employee;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "in_user")
+public class User {
+    
+}
---
+++ src/main/java/app/entity/employee/UserJobProfile.java
@@ -0,0 +1,5 @@
+package app.entity.employee;
+
+public class UserJobProfile {
+    
+}
